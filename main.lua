d0910f05f02c9b03e5fbc1b4524a9eb4c578c464
NexoModsZ <nexomodsexploit@gmail.com>
2026-01-17 22:01:57 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -168,7 +168,7 @@ NexoMods.ZIndexBehavior = Enum.ZIndexBehavior.Global
 function Env:Window(meta)
 	local Title = meta.Title or "Title"
 	local Desc = meta.Desc or "Desc"
-	local Logo = meta.Logo or 121607794580299
+	local Logo = meta.Logo or 138166244897009
 
 	local Background_1 = Instance.new("Frame")
 	local DropShadow_1 = Instance.new("ImageLabel")
